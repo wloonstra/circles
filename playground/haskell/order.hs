e73abf282c336a917c94fcd10ba46b4e9e4e86c3
Wiggert <wiggertloonstra@gmail.com>
2011-11-23 23:27:41 +0100

    changed order of declerations in haskell (playground) to have better readable overview

--- playground/haskell/order.hs
+++ playground/haskell/order.hs
@@ -1,15 +1,15 @@
 module Main where
 
+    small = ("small", 1)
+    medium = ("medium", 8)
+    big = ("big", 50)
+
+    radius :: (String, Integer) -> Integer
+    radius(name, radius) = radius
+
     sortCircles :: [Integer] -> [Integer]
     sortCircles []     = []
     sortCircles (p:xs) = (sortCircles lesser) ++ [p] ++ (sortCircles greater) where
         lesser  = filter(< p) xs
         greater = filter(>= p) xs
-
-    radius :: (String, Integer) -> Integer
-    radius(name, radius) = radius
-
-    small = ("small", 1)
-    medium = ("medium", 8)
-    big = ("big", 50)
 
