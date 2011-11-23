560b2c953cc87a0e308b26b4cbe2beb9aeb64f75
Wiggert <wiggertloonstra@gmail.com>
2011-11-23 21:20:42 +0100

    changed haskell do work with radius...

--- playground/haskell/order.hs
+++ playground/haskell/order.hs
@@ -1,5 +1,6 @@
 module Main where
-    sortCircles :: [(String, Integer)] -> [(String, Integer)]
+
+    sortCircles :: [Integer] -> [Integer]
     sortCircles []     = []
     sortCircles (p:xs) = (sortCircles lesser) ++ [p] ++ (sortCircles greater) where
         lesser  = filter(< p) xs
@@ -8,12 +9,7 @@ module Main where
     radius :: (String, Integer) -> Integer
     radius(name, radius) = radius
 
-    radiusList :: [(String, Integer)] -> [(String, Integer)]
-    radiusList[] = []
-    radiusList[(h):t] = [h] ++ (radiusList t)
-
     small = ("small", 1)
     medium = ("medium", 8)
     big = ("big", 50)
 
-    
